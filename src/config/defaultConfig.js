5cba9cf69a2fd814454bd817ea17f2a3f1ecc08b
Vyctor <dev.vyctor@gmail.com>
2020-11-15 17:43:38 -0300

    removendo token do firebase do projeto e criando placeholder

---
+++ src/config/defaultConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+export default firebase.initializeApp(firebaseConfig);
