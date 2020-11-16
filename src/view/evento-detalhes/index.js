c89d4a4fb3cca91712f1f154d4f088f742e0df5e
Vyctor <dev.vyctor@gmail.com>
2020-11-16 18:08:09 -0300

    refinando o carregamento da pagina de detalhes

--- src/view/evento-detalhes/index.js
+++ src/view/evento-detalhes/index.js
@@ -10,6 +10,8 @@ import Navbar from "../../components/navbar";
 const EventoDetalhes = (props) => {
   const [evento, setEvento] = useState();
   const [imagemUrl, setImagemUrl] = useState();
+  const [carregando, setCarregando] = useState(1);
+
   const usuarioLogado = useSelector((state) => state.usuarioEmail);
 
   useEffect(() => {
@@ -25,6 +27,11 @@ const EventoDetalhes = (props) => {
           .ref(doc.data().foto)
           .getDownloadURL()
           .then((url) => setImagemUrl(url));
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setCarregando(0);
+        }, 1000);
       });
   }, [props.match.params.id]);
 
@@ -35,55 +42,69 @@ const EventoDetalhes = (props) => {
       <Navbar />
       {evento ? (
         <div className="container-fluid">
-          <div className="row mx-auto">
-            <img
-              src={
-                imagemUrl ? imagemUrl : "https://via.placeholder.com/1000x400"
-              }
-              className="img-banner"
-              alt="Banner do evento"
-            />
-            <div className="col-12 text-right visualizacoes">
-              <i className="fa fa-eye pr-2"></i>
-              <span>{evento.visualizacoes}</span>
+          {carregando ? (
+            <div className="row mt-5">
+              <div
+                hidden={!carregando}
+                className="spinner-border text-danger mx-auto my-auto"
+                role="status"
+              ></div>
             </div>
-            <h3 className="mx-auto titulo"> {evento.titulo}</h3>
-          </div>
+          ) : (
+            <div>
+              <div className="row mx-auto">
+                <img
+                  src={
+                    imagemUrl
+                      ? imagemUrl
+                      : "https://via.placeholder.com/1000x400"
+                  }
+                  className="img-banner"
+                  alt="Banner do evento"
+                />
+                <div className="col-12 text-right visualizacoes">
+                  <i className="fa fa-eye pr-2"></i>
+                  <span>{evento.visualizacoes}</span>
+                </div>
+                <h3 className="mx-auto titulo"> {evento.titulo}</h3>
+              </div>
 
-          <div className="row justify-content-around">
-            <div className="col-md-3 col-sm-12 box-info p-3 d-flex align-items-center flex-column">
-              <i className="fas fa-ticket-alt fa-2x"></i>
-              <h5>
-                <strong>Tipo:</strong>
-              </h5>
-              <span className="mt-3">{evento.tipo}</span>
-            </div>
+              <div className="row justify-content-around">
+                <div className="col-md-3 col-sm-12 box-info p-3 d-flex align-items-center flex-column">
+                  <i className="fas fa-ticket-alt fa-2x"></i>
+                  <h5>
+                    <strong>Tipo:</strong>
+                  </h5>
+                  <span className="mt-3">{evento.tipo}</span>
+                </div>
 
-            <div className="col-md-3 col-sm-12 box-info p-3 d-flex align-items-center flex-column">
-              <i className="fas fa-calendar-alt fa-2x"></i>
-              <h5>
-                <strong>Data:</strong>
-              </h5>
-              <span className="mt-3">{evento.data}</span>
-            </div>
-          </div>
+                <div className="col-md-3 col-sm-12 box-info p-3 d-flex align-items-center flex-column">
+                  <i className="fas fa-calendar-alt fa-2x"></i>
+                  <h5>
+                    <strong>Data:</strong>
+                  </h5>
+                  <span className="mt-3">{evento.data}</span>
+                </div>
+              </div>
 
-          <div className="row box-detalhes mt-5">
-            <div className="col-12 text-center">
-              <h5>
-                <strong>Detalhes do Evento</strong>
-              </h5>
-            </div>
-            <div className="col-12 text-center">
-              <p>{evento.detalhes}</p>
-            </div>
-          </div>
+              <div className="row box-detalhes mt-5">
+                <div className="col-12 text-center">
+                  <h5>
+                    <strong>Detalhes do Evento</strong>
+                  </h5>
+                </div>
+                <div className="col-12 text-center">
+                  <p>{evento.detalhes}</p>
+                </div>
+              </div>
 
-          {usuarioLogado === evento.usuarioEmail ? (
-            <Link to="/" className="btn-editar">
-              <i className="fas fa-pen-square fa-3x"></i>
-            </Link>
-          ) : null}
+              {usuarioLogado === evento.usuarioEmail ? (
+                <Link to="/" className="btn-editar">
+                  <i className="fas fa-pen-square fa-3x"></i>
+                </Link>
+              ) : null}
+            </div>
+          )}
         </div>
       ) : null}
     </>
